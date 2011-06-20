2abf505e90189aaef171967425c515fde09965f5
Jeff Jagoda <jeffrey.jagoda@gmail.com>
2011-06-20 10:08:21 -0400

    Bug fix: cleanup rogue pid files

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import errno
 import os
 import os.path
 import signal
@@ -11,9 +12,25 @@ import gobject
 _PIDFile = os.environ['HOME'] + '/.nm_synergy.pid'
 
 
-def createPIDFile (path = _PIDFile):
+def checkPID (path):
+    if os.path.isfile(path):
+        pid = 0
+        with open(path, 'r') as file:
+            pid = int(file.read())
+
+        alive = True
+        try:
+            os.kill(pid, signal.SIGCONT)
+        except OSError as (error, stderr):
+            if error == errno.ESRCH:
+                alive = False
+
+        if alive: sys.exit()
+        else: os.remove(path)
+
+def createPIDFile (path, pid):
     with open(path, 'w') as file:
-        file.write(str(os.getpid()))
+        file.write(str(pid))
 
 def deletePIDFile (path = _PIDFile):
     os.remove(path)
@@ -22,15 +39,19 @@ def exitHandler (*args):
     deletePIDFile()
     sys.exit()
 
-def init (path = _PIDFile):
-    # FIXME: need to investigate atomic file creation in Python...
-    if os.path.isfile(path): sys.exit()
-    else: createPIDFile()
-    DBusGMainLoop(set_as_default = True)
-
 # NOTE: signal handlers need to be registered prior to main loop forking/running.
-def fork ():
-    if os.fork() == 0:
+# NOTE: signal handlers only work for "clean" exit. checkPID is used to clean up
+#   after a "dirty" exit.
+def fork (path = _PIDFile):
+    pid = os.fork()
+    if pid == 0:
         signal.signal(signal.SIGTERM, exitHandler)
         signal.signal(signal.SIGINT, exitHandler)
         gobject.MainLoop().run()
+    else:
+        createPIDFile(path, pid)
+
+def init (path = _PIDFile):
+    # FIXME: need to investigate atomic file creation in Python...
+    checkPID(path)
+    DBusGMainLoop(set_as_default = True)
